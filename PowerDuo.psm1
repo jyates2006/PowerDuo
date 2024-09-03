11ca8482bdc6d4a4377c308d7978be2c15e5b2eb
jyates2006 <jayates2006@gmail.com>
2024-09-03 15:58:54 -0400

    Add files via upload

---
+++ PowerDuo.psm1
@@ -0,0 +1,4 @@
+﻿$Public  = Get-ChildItem $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue
+$Private = Get-ChildItem $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue
+
+Foreach($Import in @($Public + $Private)) {. $Import.fullname }
